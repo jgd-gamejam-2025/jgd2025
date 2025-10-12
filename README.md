b281d9065b33b1dcf825643cb0b6d777bd0ae770
yuan <letusyooooo@gmail.com>
2025-10-11 21:12:33 -0400

    Add chat scene

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # JGD game jam 2025
-To Developers:
+This is the repo for JGD game jam 2025.
+
+## 目录结构：
 Scene 和 gdscript 放在 `scenes` 的对应子目录下，其余srcipts在`other_scripts`下：
 
-示例目录结构：
 
 ```
 scenes/
@@ -20,8 +21,13 @@ other_scripts/
 	random_script.gd
 ```
 
-This is the repo for JGD game jam 2025.
+
+## Auto Build：
 Pushing to the branch `release` will automatically export the Windows build.
 
+## LLM gguf download:
+Default: [Llama3-8B-Chinese-Chat-GGUF-4bit](https://huggingface.co/shenzhi-wang/Llama3-8B-Chinese-Chat-GGUF-4bit/tree/main)
+
+[Xwen-7B-Chat.i1-Q4_K_M](https://huggingface.co/xwen-team/Xwen-7B-Chat-i1-GGUF/blob/main/Xwen-7B-Chat.i1-Q4_K_M.gguf)
 # 3D Pixel Art Post Processing Shader for Godot 4
 This repo has the source code for the Youtube tutorial: https://youtu.be/WBoApONC7bM
---
+++ jgd2025/scenes/UI/chat_ui/chat_ui.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@onready var chatLog = $Panel/Scroll/ChatLog
+@onready var textInput = $Panel/InputHBox/TextInput
+@onready var aiChat = $NobodyWhoChat
+
+func _ready() -> void:
+	aiChat.start_worker()
+	textInput.grab_focus()
+
+func send_text_to_ai():
+	textInput.editable = false
+	chatLog.text = ""
+	aiChat.say(textInput.text)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_text_newline") and textInput.editable and textInput.text != "":
+		send_text_to_ai()
+
+func _on_nobody_who_chat_response_updated(new_token: String) -> void:
+	chatLog.text += new_token
+
+func _on_nobody_who_chat_response_finished(response: String) -> void:
+	textInput.editable = true
+	textInput.text = ""
+	# focus the text input for next message
+	textInput.grab_focus()
+
+func show_text_gradually(full_text: String, interval: float = 0.05) -> void:
+	chatLog.text = ""
+	var char_index := 0
+	var char_count := full_text.length()
+	while char_index < char_count:
+		chatLog.text += full_text[char_index]
+		char_index += 1
+		await get_tree().create_timer(interval).timeout

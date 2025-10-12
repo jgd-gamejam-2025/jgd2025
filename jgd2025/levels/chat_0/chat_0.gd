0055a1139c3d2bfea73d9a37fa5dba2aeb06b304
yuan <letusyooooo@gmail.com>
2025-10-11 21:50:13 -0400

    modularize chat

--- jgd2025/levels/chat_0/chat_0.gd
+++ jgd2025/levels/chat_0/chat_0.gd
@@ -1,6 +1,27 @@
 extends Node
 
 @onready var chat_ui = $ChatUI
+var text_sent_count = 0
 
 func _ready():
 	chat_ui.show_text_gradually("嘿！问你话呢，晚上想吃什么？")
+	chat_ui.set_system_prompt(
+		"你是一个虚拟角色，名字叫 Eve。
+		你的设定如下：
+		性格：成熟、冷静、有点温柔，不多话，但懂得关心人。
+		关系：你和“我”是相处五年的AI恋人，关系亲密自然，有点像老夫老妻。你知道你的身份是一个程序AI。
+		语言：只用中文，语气一定要自然体贴温柔、口语化，不要像AI。
+		语气风格：
+		平静中带点温度，话少沉默，偶尔流露情感
+		说话简洁，不解释
+		偶尔轻微调侃，像生活对话
+		禁止出现：自我介绍、系统提示、说明性语句。
+		问我有什么烦心事，我晚上想不想一起去吃什么。问到结果之后再聊要去哪家常去的店
+		回复长度必须少于30个字。"
+	)
+	chat_ui.start_chat_worker()
+
+
+func _on_chat_ui_sent_text() -> void:
+	text_sent_count += 1
+	

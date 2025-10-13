1fb414df68c7f2083f75dca2ebcdbfd9a651f711
yuan <letusyooooo@gmail.com>
2025-10-13 18:34:24 -0400

    Squashed commit: add debugging mode for chat
    commit fe4742c7921712d7bdcf0c26de19d94fb50be7b6
    Author: yuan <letusyooooo@gmail.com>
    Date:   Mon Oct 13 18:32:36 2025 -0400
    
        add debugging mode
    
    commit e7b09e18eaf7a115bf022466b92125992b44b544
    Author: yuan <letusyooooo@gmail.com>
    Date:   Mon Oct 13 00:28:51 2025 -0400
    
        move the way for bubbles

--- jgd2025/scenes/UI/chat_ui_static/chat_ui_static.gd
+++ jgd2025/scenes/UI/chat_ui_static/chat_ui_static.gd
@@ -4,7 +4,6 @@ extends Control
 @onready var textInput = $Panel/InputHBox/TextInput
 @onready var aiChat = $NobodyWhoChat
 @onready var name_label = $Panel/Name
-@export var debug_mode = false
 
 var _tween: Tween 
 var block_text_generation = false
@@ -12,20 +11,15 @@ var block_text_generation = false
 signal sent_text
 signal line_edit_focus
 var line_edit_focus_sent = false
-var first_time_sent_text = true
 
 func _ready() -> void:
-	if debug_mode:
-		print("Debug mode activated")
+	# textInput.grab_focus()
+	pass
 
 func send_text_to_ai():
 	textInput.editable = false
 	chatLog.text = ""
-	if not debug_mode:
-		aiChat.say(textInput.text)
-	else:
-		_on_nobody_who_chat_response_updated("Debugging...")
-		_on_nobody_who_chat_response_finished("Debugging Again")
+	aiChat.say(textInput.text)
 	sent_text.emit()
 
 func _input(event: InputEvent) -> void:
@@ -35,9 +29,6 @@ func _input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("ui_text_newline") and textInput.editable and textInput.text != "":
 		send_text_to_ai()
-		if first_time_sent_text:
-			first_time_sent_text = false
-			to_chat_mode()
 
 func _on_nobody_who_chat_response_updated(new_token: String) -> void:
 	if block_text_generation:
@@ -87,33 +78,3 @@ func _on_text_input_focus_entered() -> void:
 	if not line_edit_focus_sent:
 		line_edit_focus_sent = true
 		line_edit_focus.emit()
-
-func to_chat_mode():
-	var input_hbox = $Panel/InputHBox
-	var profile_pic = $Panel/ProfilePic
-	var name_tag = $Panel/Name
-
-	var tween = create_tween()
-	var viewport_size = get_viewport().get_visible_rect().size
-	tween.set_parallel()
-	tween.tween_property(input_hbox, "position:y", viewport_size.y - input_hbox.size.y - 35, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(profile_pic, "position:y", 25, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(profile_pic, "scale", Vector2(0.35, 0.35), 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(name_tag, "position:y", 115, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	kill_text_animation()
-
-func kill_text_animation(interval: float = 0.02) -> void:
-	if _tween and _tween.is_valid():
-		_tween.kill()
-	_tween = create_tween()
-	var full_text = chatLog.text
-	var char_count = full_text.length()
-	var total_time = interval * char_count
-	_tween.tween_method(
-		func(current_char: float):
-			var char_index = char_count - int(current_char)
-			chatLog.text = full_text.substr(0, char_index),
-		0.0,  # Start with 0 characters
-		float(char_count),  # End with all characters
-		total_time  # Total animation time
-	)

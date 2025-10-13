3cd781f73957d267bd15705244f923e46ce4e489
yuan <letusyooooo@gmail.com>
2025-10-13 00:28:51 -0400

    move the way for bubbles

--- jgd2025/scenes/UI/chat_ui_static/chat_ui_static.gd
+++ jgd2025/scenes/UI/chat_ui_static/chat_ui_static.gd
@@ -12,8 +12,6 @@ signal sent_text
 signal line_edit_focus
 var line_edit_focus_sent = false
 
-var first_time_sent_text = true
-
 func _ready() -> void:
 	# textInput.grab_focus()
 	pass
@@ -31,9 +29,6 @@ func _input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("ui_text_newline") and textInput.editable and textInput.text != "":
 		send_text_to_ai()
-		if first_time_sent_text:
-			first_time_sent_text = false
-			to_chat_mode()
 
 func _on_nobody_who_chat_response_updated(new_token: String) -> void:
 	if block_text_generation:
@@ -83,33 +78,3 @@ func _on_text_input_focus_entered() -> void:
 	if not line_edit_focus_sent:
 		line_edit_focus_sent = true
 		line_edit_focus.emit()
-
-func to_chat_mode():
-	var input_hbox = $Panel/InputHBox
-	var profile_pic = $Panel/ProfilePic
-	var name_tag = $Panel/Name
-
-	var tween = create_tween()
-	var viewport_size = get_viewport().get_visible_rect().size
-	tween.set_parallel()
-	tween.tween_property(input_hbox, "position:y", viewport_size.y - input_hbox.size.y - 35, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(profile_pic, "position:y", 25, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(profile_pic, "scale", Vector2(0.35, 0.35), 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(name_tag, "position:y", 115, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	kill_text_animation()
-
-func kill_text_animation(interval: float = 0.02) -> void:
-	if _tween and _tween.is_valid():
-		_tween.kill()
-	_tween = create_tween()
-	var full_text = chatLog.text
-	var char_count = full_text.length()
-	var total_time = interval * char_count
-	_tween.tween_method(
-		func(current_char: float):
-			var char_index = char_count - int(current_char)
-			chatLog.text = full_text.substr(0, char_index),
-		0.0,  # Start with 0 characters
-		float(char_count),  # End with all characters
-		total_time  # Total animation time
-	)

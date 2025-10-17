05e1df95be5d73d56a34a1a65cb4798e00b567a6
Yuan Fang <ry.fang@outlook.com>
2025-10-17 02:13:03 -0400

    change pad config

--- jgd2025/levels/fps_test_scene/fps_test_scene.gd
+++ jgd2025/levels/fps_test_scene/fps_test_scene.gd
@@ -30,4 +30,5 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		# Transition.set_and_start("正在尝试重新连接……", "")
 		# await get_tree().create_timer(0.7).timeout
 		# get_tree().change_scene_to_packed(next_scene)
-		$room.hide()
+		#$room.hide()
+		pass

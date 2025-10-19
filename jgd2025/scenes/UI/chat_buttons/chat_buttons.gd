0fb7e48faac9284fbe5336c5bf2def2f6a2a852c
Yuan <ry.fang@outlook.com>
2025-10-19 02:17:08 -0400

    add buttons

---
+++ jgd2025/scenes/UI/chat_buttons/chat_buttons.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+@export var is_3d_scene = false
+
+func _ready():
+	hide()
+	if is_3d_scene:
+		$Background.show()
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		if visible:
+			if is_3d_scene:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			hide()
+		else:
+			show()
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()

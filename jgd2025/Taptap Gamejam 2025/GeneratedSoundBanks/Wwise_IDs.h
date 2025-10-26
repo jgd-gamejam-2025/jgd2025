cf42447d906a2d7fdcc7a9c525f3201e21f5589e
PYG-yaya <sarahpeng0408@gmail.com>
2025-10-25 21:05:54 -0400

    change Event name

--- jgd2025/Taptap Gamejam 2025/GeneratedSoundBanks/Wwise_IDs.h
+++ jgd2025/Taptap Gamejam 2025/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,24 +13,24 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BEGIN_TO_NONE = 3907958213U;
-        static const AkUniqueID MAZE_LANDING = 2598202372U;
-        static const AkUniqueID NONE_TO_TITLE = 4080990482U;
-        static const AkUniqueID PLAU_AI_TYPING_FAULT = 2435819095U;
-        static const AkUniqueID PLAY_AI_TYPING = 3734422766U;
+        static const AkUniqueID MX_BEGIN_TO_NONE = 1069070997U;
+        static const AkUniqueID MX_PLAY_BEGIN = 891883193U;
+        static const AkUniqueID MX_PLAY_TITLE = 2665577840U;
+        static const AkUniqueID MX_TITLE_TO_MAZE = 2331460791U;
         static const AkUniqueID PLAY_AMB_MAZE = 2190478052U;
         static const AkUniqueID PLAY_AMB_ROOM = 1075867404U;
-        static const AkUniqueID PLAY_BEGIN = 2375588297U;
-        static const AkUniqueID PLAY_PILLARUP = 679375753U;
-        static const AkUniqueID PLAY_PLAYER_ENTER = 2025015394U;
-        static const AkUniqueID PLAY_PLAYER_TYPING = 88175825U;
-        static const AkUniqueID PLAY_PUTDOWN_IPAD = 2538431492U;
-        static const AkUniqueID PLAY_ROOM_FOOTSTEPS = 2910264647U;
-        static const AkUniqueID PLAY_TAKEOUT_IPAD = 2792899676U;
         static const AkUniqueID SET_AMB_ROOM = 3481726148U;
+        static const AkUniqueID SFX_MAZE_LANDING = 4187727620U;
         static const AkUniqueID SFX_PICKUP_NEWSPAPER = 3066694657U;
+        static const AkUniqueID SFX_PLAY_AI_TYPING = 3636131822U;
+        static const AkUniqueID SFX_PLAY_AI_TYPING_FAULT = 693053491U;
+        static const AkUniqueID SFX_PLAY_PILLARUP = 1307923593U;
+        static const AkUniqueID SFX_PLAY_PLAYER_TYPING = 1312151505U;
+        static const AkUniqueID SFX_PLAY_PLR_TYPING_RETURN = 4078102291U;
+        static const AkUniqueID SFX_PLAY_PUTDOWN_IPAD = 26301700U;
+        static const AkUniqueID SFX_PLAY_ROOM_FOOTSTEPS = 2251224647U;
+        static const AkUniqueID SFX_PLAY_TAKEOUT_IPAD = 280769884U;
         static const AkUniqueID SFX_PUTDOWN_NEWSPAPER = 4019675232U;
-        static const AkUniqueID TITLE_TO_MAZE = 130021575U;
     } // namespace EVENTS
 
     namespace STATES
